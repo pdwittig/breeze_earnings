a3efb9d0329dff2f361c7790457ae2b81239a8a8
Phil Wittig <pdwittig@gmail.com>
2014-09-17 19:32:52 -0700

    generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BreezeEarnings::Application.config.secret_key_base = '4e30889a2effafc3bb96fcf66dee319dae386c613146f484bb92f70da6a3225d9adcc2f6eecda58af2b079a1c90fa01628c13e8b571a2f2cee9435f7b211ae26'
